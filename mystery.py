137b11056a12ff3cb785e69f6d1a309c5794e6a9
sarah-wines <sarah.lodico.wines@gmail.com>
2021-08-06 16:42:50 -0400

    fills in letter at index

--- mystery.py
+++ mystery.py
@@ -8,7 +8,12 @@
 
 mystery_word = "kitty"
 print(mystery_word)
-print("_ " * len(mystery_word))
+gameboard = ["_"] * len(mystery_word)
+print(gameboard)
 print("Your word has " + str(len(mystery_word)) + " letters.")
 guess = input("Please guess a letter, you have 8 guesses ")
 print(guess)
+if guess in mystery_word:
+    index = mystery_word.index(guess)
+    gameboard[index] = guess
+    print(gameboard)
