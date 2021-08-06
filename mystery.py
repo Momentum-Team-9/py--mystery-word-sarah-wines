a9ab2be39ec8ccbbee49dac54198b49d6f07a6cc
sarah-wines <sarah.lodico.wines@gmail.com>
2021-08-06 15:55:33 -0400

    prints word with blanks and requests user input

---
+++ mystery.py
@@ -0,0 +1,14 @@
+# choose difficulty: easy(4-6), normal(6-8), hard(8+)
+# sort words by length, select words from list within range of level
+# ensure random word does not change
+
+# ???import the random module
+# ???import random
+
+
+mystery_word = "kitty"
+print(mystery_word)
+print("_ " * len(mystery_word))
+print("Your word has " + str(len(mystery_word)) + " letters.")
+guess = input("Please guess a letter, you have 8 guesses ")
+print(guess)
