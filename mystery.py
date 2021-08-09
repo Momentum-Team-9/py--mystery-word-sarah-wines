193a0e07a39f796416a1efd9adbfbfb9beffd8f8
sarah-wines <sarah.lodico.wines@gmail.com>
2021-08-08 23:48:30 -0400

    handles double letter, refactors code, ascii art

--- mystery.py
+++ mystery.py
@@ -6,6 +6,9 @@
 # ???import random
 import random
 
+# declares needed global variable
+mystery_word = ""
+
 
 def mysteryword():
     with open("words.txt") as words_list:
@@ -20,67 +23,85 @@ def mysteryword():
             normal_words.append(word)
         elif len(word) >= 8:
             hard_words.append(word)
-    level = input("Easy, normal, or hard? ")
-    if level == "easy":
-        mystery_word = random.choice(easy_words).lower()
-    elif level == "normal":
-        mystery_word = random.choice(normal_words).lower()
-    elif level == "hard":
-        mystery_word = random.choice(hard_words).lower()
-    gameboard = ["_"] * len(mystery_word)
-    # gameover = False
-    guess_another = "yes"
     guess_count = 0
     wrong_guesses = []
+
+    # sets mystery word/level based on input
+    def start_game():
+        global mystery_word
+        level = input("Easy, normal, or hard? ")
+        if level == "easy":
+            mystery_word = random.choice(easy_words).lower()
+        elif level == "normal":
+            mystery_word = random.choice(normal_words).lower()
+        elif level == "hard":
+            mystery_word = random.choice(hard_words).lower()
+        # reassigns global variable
+
+    guess_another = "yes"
+    start_game()
+    gameboard = ["_"] * len(mystery_word)
+    print("Your word has " + str(len(mystery_word)) + " letters.")
     print(mystery_word)
     while guess_another == "yes":
         print(" ".join(gameboard))
-        print("Your word has " + str(len(mystery_word)) + " letters.")
         guess = input("Please guess a letter ")
-        # print(guess)
-        # while gameover is False:
+
         if guess in mystery_word:
-            index = mystery_word.index(guess)
-            gameboard[index] = guess
-            # print(" ".join(gameboard))
+            start = 0
+            while mystery_word.find(guess, start) != -1:
+                index = mystery_word.index(guess, start)
+                gameboard[index] = guess
+                start = index + 1
         else:
             if guess in wrong_guesses:
-                print("You've already guessed '" + guess + "' try another letter.")
+                print("You've already guessed '" + guess
+                      + "' try another letter.")
             else:
                 guess_count += 1
                 print("That letter is not in the word.")
-                print("You have " + (str(8 - guess_count)) + " guesses remaining.")
+                print("You have " + (str(8 - guess_count))
+                      + " guesses remaining.")
                 wrong_guesses.append(guess)
         if mystery_word == ("".join(gameboard)):
-            play_again = input("You win! Would you like to play again? ")
+            print("")
+            print("                     /|")
+            print("       =  =  =      / |")
+            print("  ____| || || |____/  | -_-_-_-_-_-_")
+            print("|)----| || || |____   | YOU WIN!!!")
+            print("  ((  | || || |  ))\  | _-_-_-_-_-_-")
+            print("   \\\_|_||_||_|_//  \ |")
+            print("    \___________/    \|")
+            print("")
+            play_again = input("Would you like to play again? ")
             if play_again == "no":
-                quit()
+                guess_another = "stop"
             if play_again == "yes":
-                level = input("Easy, normal, or hard? ")
-            if level == "easy":
-                mystery_word = random.choice(easy_words).lower()
-            elif level == "normal":
-                mystery_word = random.choice(normal_words).lower()
-            elif level == "hard":
-                mystery_word = random.choice(hard_words).lower()
-            gameboard = ["_"] * len(mystery_word)
-            guess_count = 0
-            wrong_guesses = []
+                start_game()
+                guess_count = 0
+                gameboard = ["_"] * len(mystery_word)
+                print("Your word has " + str(len(mystery_word)) + " letters.")
         if guess_count == 8:
-            play_again = input("You lose. Your word was '" + mystery_word + "' Would you like to play again? ")
+            print("")
+            print("     .-''''''-.  ")
+            print("   .'          '. ")
+            print("  |   O      O   |")
+            print(" :           `    :")
+            print(" |                |   ")
+            print(" :    .------.    :")
+            print("  |  '        '  |")
+            print("   '.          .'")
+            print("     '-......-'")
+            print("")
+            play_again = input("You lose. Your word was '" + mystery_word
+                               + "' Would you like to play again? ")
             if play_again == "no":
-                quit()
+                guess_another = "stop"
             if play_again == "yes":
-                level = input("Easy, normal, or hard? ")
-            if level == "easy":
-                mystery_word = random.choice(easy_words).lower()
-            elif level == "normal":
-                mystery_word = random.choice(normal_words).lower()
-            elif level == "hard":
-                mystery_word = random.choice(hard_words).lower()
-            gameboard = ["_"] * len(mystery_word)
-            guess_count = 0
-            wrong_guesses = []
-            
+                start_game()
+                guess_count = 0
+                gameboard = ["_"] * len(mystery_word)
+                print("Your word has " + str(len(mystery_word)) + " letters.")
+
 
 mysteryword()
